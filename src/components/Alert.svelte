ad83b6eb8fd50ffe1d1009d9d66bb1e5c3542418
Margus Liinev <margus.liinev@gmail.com>
2024-02-24 20:00:55 +0200

    update alert api, make title optional, add description as children

--- src/components/Alert.svelte
+++ src/components/Alert.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
     import type { HTMLAttributes } from 'svelte/elements'
+    import type { Snippet } from 'svelte'
     import { twJoin, twMerge } from 'tailwind-merge'
     import { Icon } from '$components'
 
     interface AlertProps extends HTMLAttributes<HTMLDivElement> {
+        children: Snippet
         variant: 'success' | 'warning' | 'danger' | 'info'
-        title: string
-        description: string
+        title?: string
     }
 
-    let { variant, title, description, class: className, ...props } = $props<AlertProps>()
+    let { children, variant, title, class: className, ...props } = $props<AlertProps>()
 
     let alertVariant = {
         success: 'border-success/30 text-success bg-success/10',
@@ -17,8 +18,8 @@
         danger: 'border-danger/30 text-danger bg-danger/10',
         info: 'border-info/30 text-info bg-info/10'
     }
-    let alertCore = 'relative text-sm w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg]:absolute'
-    let alertStyles = twJoin(alertCore, alertVariant[variant])
+    let alertCore = 'text-sm w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg]:absolute'
+    let alertStyles = twJoin(alertCore, alertVariant[variant ?? 'danger'])
 </script>
 
 <div {...props} role="alert" class={twMerge(alertStyles, className)}>
@@ -34,6 +35,8 @@
     {#if variant === 'info'}
         <Icon name="info" size="sm" />
     {/if}
-    <h5 class="mb-1 font-semibold capitalize dark:font-medium">{title}</h5>
-    <p>{description}</p>
+    {#if title}
+        <h5 class="mb-1 font-medium capitalize">{title}</h5>
+    {/if}
+    <p>{@render children()}</p>
 </div>
--- src/routes/home/alert/+page.svelte
+++ src/routes/home/alert/+page.svelte
@@ -9,8 +9,17 @@
     ] as const
 </script>
 
-<div class="grid gap-4">
-    {#each variants as variant, i}
-        <Alert {variant} title={variant !== 'danger' ? variant : 'error'} description={`${descriptions[i]}`} />
-    {/each}
+<div class="grid gap-4 lg:grid-cols-2">
+    <div class="flex flex-col gap-4">
+        <h2 class="font-medium">With Title</h2>
+        {#each variants as variant, i}
+            <Alert {variant} title={variant}>{descriptions[i]}</Alert>
+        {/each}
+    </div>
+    <div class="flex flex-col gap-4">
+        <h2 class="font-medium">Without Title</h2>
+        {#each variants as variant, i}
+            <Alert {variant}>{descriptions[i]}</Alert>
+        {/each}
+    </div>
 </div>
