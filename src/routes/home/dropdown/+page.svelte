65b0330a35f45fd3920a1130371c83f2d52679c5
Margus Liinev <margus.liinev@gmail.com>
2024-02-29 23:57:44 +0200

    add 3 column dropdown example

--- src/routes/home/dropdown/+page.svelte
+++ src/routes/home/dropdown/+page.svelte
@@ -3,11 +3,29 @@
 </script>
 
 <div class="flex flex-wrap items-center justify-between gap-4">
+    <Dropdown class="w-fit">
+        <DropdownButton class="border border-border bg-background shadow-sm transition-none" icon><Icon name="user" size="sm" /><span>John Doe</span></DropdownButton>
+        <DropdownMenu>
+            <DropdownItem><Link href="#" size="sm" class="flex justify-start rounded-none hover:bg-foreground/10">Your account</Link></DropdownItem>
+            <DropdownItem><Link href="#" size="sm" class="flex justify-start rounded-none hover:bg-foreground/10">Settings</Link></DropdownItem>
+            <DropdownItem><Form><Button style="plain" size="sm" class="flex justify-start rounded-none hover:bg-foreground/10">Logout</Button></Form></DropdownItem>
+        </DropdownMenu>
+    </Dropdown>
+
     <Dropdown>
         <DropdownButton class="border border-border bg-background shadow-sm transition-none" icon>Two Columns</DropdownButton>
         <DropdownMenu class="grid w-max grid-cols-2">
             <DropdownItem><Link href="#" size="sm" class="flex justify-start rounded-none hover:bg-foreground/10">Pricing</Link></DropdownItem>
             <DropdownItem><Link href="#" size="sm" class="flex justify-start rounded-none hover:bg-foreground/10">About</Link></DropdownItem>
+            <DropdownItem><Link href="#" size="sm" class="flex justify-start rounded-none hover:bg-foreground/10">Privacy</Link></DropdownItem>
+            <DropdownItem><Link href="#" size="sm" class="flex justify-start rounded-none hover:bg-foreground/10">Security</Link></DropdownItem>
+        </DropdownMenu>
+    </Dropdown>
+
+    <Dropdown>
+        <DropdownButton class="border border-border bg-background shadow-sm transition-none" icon>Three Columns</DropdownButton>
+        <DropdownMenu class="grid w-max grid-cols-3">
+            <DropdownItem><Link href="#" size="sm" class="flex justify-start rounded-none hover:bg-foreground/10">Pricing</Link></DropdownItem>
             <DropdownItem><Link href="#" size="sm" class="flex justify-start rounded-none hover:bg-foreground/10">Privacy</Link></DropdownItem>
             <DropdownItem><Link href="#" size="sm" class="flex justify-start rounded-none hover:bg-foreground/10">Security</Link></DropdownItem>
         </DropdownMenu>
@@ -20,15 +38,6 @@
             <DropdownItem><Link href="#" size="sm" class="flex justify-start rounded-none hover:bg-foreground/10">About</Link></DropdownItem>
             <DropdownItem><Link href="#" size="sm" class="flex justify-start rounded-none hover:bg-foreground/10">Privacy</Link></DropdownItem>
             <DropdownItem><Link href="#" size="sm" class="flex justify-start rounded-none hover:bg-foreground/10">Security</Link></DropdownItem>
-        </DropdownMenu>
-    </Dropdown>
-
-    <Dropdown class="w-fit">
-        <DropdownButton class="border border-border bg-background shadow-sm transition-none" icon><Icon name="user" size="sm" /><span>John Doe</span></DropdownButton>
-        <DropdownMenu>
-            <DropdownItem><Link href="#" size="sm" class="flex justify-start rounded-none hover:bg-foreground/10">Your account</Link></DropdownItem>
-            <DropdownItem><Link href="#" size="sm" class="flex justify-start rounded-none hover:bg-foreground/10">Settings</Link></DropdownItem>
-            <DropdownItem><Form><Button style="plain" size="sm" class="flex justify-start rounded-none hover:bg-foreground/10">Logout</Button></Form></DropdownItem>
         </DropdownMenu>
     </Dropdown>
 </div>
