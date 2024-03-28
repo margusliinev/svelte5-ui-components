7e9557b885478c455791fea7821bae2eabaecdfc
Margus Liinev <margus.liinev@gmail.com>
2024-03-28 18:14:31 +0200

    add forms and formfields

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,8 +4,12 @@ import Button from './Button.svelte';
 import ButtonSpinner from './ButtonSpinner.svelte';
 import Card from './Card.svelte';
 import Form from './Form.svelte';
+import FormField from './FormField.svelte';
+import Input from './Input.svelte';
+import InputError from './InputError.svelte';
+import Label from './Label.svelte';
 import Icon from './Icon.svelte';
 import Link from './Link.svelte';
 import ThemeSwitch from './ThemeSwitch.svelte';
 
-export { Alert, Badge, Button, ButtonSpinner, Card, Form, Icon, Link, ThemeSwitch };
+export { Alert, Badge, Button, ButtonSpinner, Card, Form, FormField, Input, InputError, Label, Icon, Link, ThemeSwitch };
